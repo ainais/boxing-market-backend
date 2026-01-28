9a9d472cedd4fc59cf0f6e0a670fd4931ebbc0f6
ainais <kou8148@naver.com>
2026-01-28 18:32:16 +0900

    init project

---
+++ src/main/java/com/ainais/boxingmarket/BoxingMarketApplication.java
@@ -0,0 +1,13 @@
+package com.ainais.boxingmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxingMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoxingMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ainais/boxingmarket/BoxingMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ainais.boxingmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxingMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
